7471c0a600178543bea548f58cc0b6e9655ed122
ppbao <bruce.liu.02@gmail.com>
2017-01-01 09:04:42 +1000

    commit it

--- app/src/main/java/au/com/unisharing/t4/MainActivity.java
+++ app/src/main/java/au/com/unisharing/t4/MainActivity.java
@@ -2,6 +2,7 @@ package au.com.unisharing.t4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("TAG", "onCreate: ");
+        Log.i("TAG", "onCreate: ");
+
     }
 }
